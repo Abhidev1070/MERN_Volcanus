c118d0452295d81490f899884bc6050b44d21668
Abhidev1070 <abhishekdewda506@gmail.com>
2024-02-06 12:59:52 +0530

    variable , template

---
+++ day30_js_bacis/script.js
@@ -0,0 +1,43 @@
+console .log("Hello World")
+// {
+//     5+2
+// }
+// let number1 = 10;
+// let number2 = 11
+// let number =30.2;
+
+// console.log(number1 + number2);
+// console.log(number)
+// let num ='A'
+// console.log(num)
+
+// let Name = "Abhishek"
+// console.log(Name)
+// let is18 = false 
+// console.log(is18) 
+
+// let $do = 3000
+// console.log("i have " +$do+ " $")
+
+// let name1 = "superman"
+// let name2 = "spiderman"
+// console.log(+name1+"is blong form DC and " +name2+ "bjvbuichedgo")
+
+
+// let name1 = "superman"
+// let name2 = "spiderman"
+
+//  name1 = "abhi"
+// console.log(`"${name1}" is belong from "DC" and "${name2}" is belong from `)
+// const num = 20;
+// num=30;
+// console.log(num)
+// let number1 = 20;
+// let number2 = 30;
+// console.log(number1++, number1++,number1++)
+const number = 20
+// doible equal (==) is use to check only value of variable
+console.log(number == 20)
+typeof(number)
+
+// console.log(typeof number)
