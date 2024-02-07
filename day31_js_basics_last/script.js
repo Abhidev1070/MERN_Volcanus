62ebcf0ff2e634fe7c38d0940fc20159d314ce0b
Abhidev1070 <abhishekdewda506@gmail.com>
2024-02-07 13:01:41 +0530

    script js

---
+++ day31_js_basics_last/script.js
@@ -0,0 +1,84 @@
+// const age = 18
+// if(age>18){
+//     console.log("u can drive")
+// }
+// else{
+//     console.log("you can't drive")
+// }
+// const age = 18
+// const age = prompt("Enter your age ")
+
+// console.log ("user enter age ",age )
+
+// if(age>18)
+// {
+//     const city = prompt("Enter you city ");
+//     if( city != "indore")
+//     console.log("u can drive")
+// }
+// else{
+//     console.log('u can drive')
+// }
+// else{
+//     console.log("you can't drive")
+// }
+
+// const $dollar = 500;
+// if ($dollar >= 500) {
+//     console.log("you can purchase iphone")
+// }
+// else {
+//     console.log("you can purchase  not iphone")
+
+// }
+// ($dollar <500)
+//  ? console.log("you can purchase iphone")   
+// : console.log("you are not purchase ")
+
+// const day = parseInt(prompt("Enter day"));
+
+// switch(day){
+
+//     case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//         console.log("wednesday");
+//         break;
+//         case 4:
+//         console.log("thursday");
+//         break;
+//         case 5:
+//         console.log("friday");
+//         break;
+//         case 6:
+//         console.log("saturday");
+//         break;
+//         case 7:
+//         console.log("sunday / holiday");
+//         break;
+// }
+
+
+
+//normal function declaration
+
+
+
+function print(){
+    console.log("this is function body");
+}
+print();
+
+
+////
+
+function pi(){
+    return 3.14
+}
+// console.log(pi());
+const Abhi = pi();
+console.log(Abhi);
