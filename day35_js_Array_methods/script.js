2a9034cc90173987d68bd67a71add8126787939e
Abhidev1070 <abhishekdewda506@gmail.com>
2024-02-14 12:09:29 +0530

    Array

---
+++ day35_js_Array_methods/script.js
@@ -0,0 +1,19 @@
+// let arr = [10,20,30,40,50,80];
+// it will update the original
+// arr.splice(1,2);
+
+// console.log(arr);
+// slice return a barnd new array ,we have to pass the  start end index of element ,it will exclude tha last index
+// let slicedArray = arr.slice(2,3);
+
+// arr.slice(1,2)
+// console.log("sliced Array ",slicedArray);
+// let arr1 = ["laptop","smartphones","smartwatch","ipad"];
+// let arr2 =["orange", "bananan","cherry","mango"];
+// let result = arr1.concat(arr2);
+// console.log(result);
+
+const arr = ["orange", "bananan","cherry","mango" ,"Appal"];
+console.log("index of searching element = ",arr.indexOf("Appal") );
+const searchingElement = "Appal"
+console.log(" last index of searching element = ",arr.lastIndexOf("bananan") );
