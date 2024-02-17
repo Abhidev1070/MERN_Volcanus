c343bc7c945bf483c3523d9478678c37e663d7dc
Abhidev1070 <abhishekdewda506@gmail.com>
2024-02-17 13:36:55 +0530

    Test_5

---
+++ Test_17_2_24/script.js
@@ -0,0 +1,80 @@
+////  1st Q
+
+// let arr = [10,20,30,40,50,60,]
+// const nawElemant =20;
+// const removsamenumber = () => {
+//     for(let i=0; i<arr.length;i++){
+//         if (nawElemant == arr[i]){
+//             arr.splice([i],i)
+//             console.log("After arr" , arr);
+//         }
+//     }
+// }
+// removsamenumber ()
+// arr.push(nawElemant)
+// console.log("After pushing",arr);
+//// 2nd Q
+// const stringArray = ["sahil","anurag","sumansir","ashish","gourav","anurag"];
+// const searchElement="SUMANSIR";
+// console.log(stringArray.includes(searchElement.toLowerCase()),"(Therefore it is already exist in the array.)");
+// const newElement="narendra";
+// console.log(stringArray.includes(newElement),"(Therefore this string is not present in the element)");
+// stringArray.push("narendra");
+// console.log(stringArray);
+
+//// 3ed Q
+
+// const movies = [    {
+//         id:1,
+//         Name:"human",
+//         rating:"7.5",
+//         popular: "yutha",
+//         actor:"Teja",
+//         song:"hauman"
+
+//     },
+//     {
+//         id:1,
+//         Name:"human",
+//         rating:"7.5",
+//         popular: "yutha",
+//         actor:"Teja",
+//         song:"hauman"
+
+//     },
+//     {
+//         id:2,
+//         Name:"Milkha",
+//         rating:"9",
+//         popular: "Athelit",
+//         actor:"Faranakhater",
+//         song:"Bhag milkha"
+
+//     },
+//     {
+//         id:3,
+//         Name:"KGF",
+//         rating:"9",
+//         popular: "rock",
+//         actor:"yash",
+//         song:"maa"
+
+//     },
+// ]
+// console.log(movies);
+// // //const filterby = "milkha"
+// const filterByName = movies.filter((movies) =>movies.Name.toLocaleLowerCase().includes("name,id"))
+// console.log(filterByName);
+// const filterByid = movies.filter((movies) =>movies.Name.toLocaleLowerCase().includes("name,id"))
+// console.log(filterByName);
+// const filterByrating = movies.filter((movies) =>movies.Name.toLocaleLowerCase().includes("name,id"))
+// console.log(filterByName);
+// const filterBypopular = movies.filter((movies) =>movies.Name.toLocaleLowerCase().includes("name,id"))
+// console.log(filterByName);
+// const filterByactor = movies.filter((movies) =>movies.Name.toLocaleLowerCase().includes("name,id"))
+// console.log(filterByName);
+
+
+
+
+
