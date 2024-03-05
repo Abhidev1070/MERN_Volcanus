3594e627b143eb7aedff955075201dc4e12d1429
Abhidev1070 <abhishekdewda506@gmail.com>
2024-03-05 13:12:42 +0530

    BOMS

---
+++ day43_js_14_BOM_Methos/app.js
@@ -0,0 +1,20 @@
+// console.log("javascript Browser object");
+// console.log(window);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+// const innerHeight = window.innerHeight;
+// const innerWidth = window.innerWidth;
+
+// document.querySelector('.superman').innerHTML=`<h1> browser innerHeight = ${innerHeight}</h1> 
+// <h1> ${innerWidth}</h1>`
+
+// //href
+// const href = window.location.href
+// const hostname = window.location.hostname
+
+// document.querySelector(".bom").innerHTML = `
+// <h3> href = ${href}</h3>
+// <h3> Host Name =${hostname}</h3>
+// <h3> origin = ${window.location.origin}</h3>
+// `
