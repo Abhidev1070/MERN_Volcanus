aa59c5c7c58bc14f34d759e3f08075c1c4cf70fb
Abhidev1070 <abhishekdewda506@gmail.com>
2024-03-13 13:08:52 +0530

    oops class object

---
+++ day49_js_20_constrcut/App.js
@@ -0,0 +1,71 @@
+// console.log('HELLO Console');
+// const user ={
+//     username: 'Abhi',
+//     age:400,
+//     salary:41258,
+//     loginCount:8,
+//     signedIn:true,
+//     getUserDetails:function (){
+//         // console.log("got user details from database");
+//         // console.log(`Username:${this.username}`);
+//         // console.log(this);
+//         return this
+//     }
+// }
+// // console.log(user.username);
+// // console.log(user.getUserDetails());
+// // console.log(this);
+// console.log(user);
+
+// const p = new Promise();
+
+// const data = new data ();e
+
+// function product (name,price,quentity){
+//    this. name =name ,
+//     this.price =price
+//     return this
+
+// }
+// // console.log(product(macbook,12345678,2));
+// const p1 =   new product('macbook',12345678,2);
+// const p2 =   new product('hp',258639,9);
+
+// product.prototype.brandName  = 'Apple';
+// product.prototype.quentity  = 20;
+// product.prototype.price  = 30;
+// product.prototype.shell  = function(){
+//     return 20;
+// }
+// console.log(p1.brandName,p1.price);
+// console.log(p1.shell());
+
+// console.log(p1);
+// console.log(p2);
+
+
+class car {
+constructor ( brandName,price,color){
+    this.brandName = brandName
+    this.price=price
+    this.color=color
+}
+display(){
+    console.log(` The Car Brand Name = ${this.brandName} and price-= ${this.price} and color = ${this.color}`);
+}
+}
+class BMW  extends car{
+    constructor(wheels,power,brandName,price,color){
+        super(brandName,price,color)
+        this.wheels =wheels;
+        this.power =power;
+    }
+    display(){
+        console.log(` The Car Brand Name = ${this.brandName} and price-= ${this.price} and color = ${this.color} wheels = ${this.wheels} power = ${this.power}`);
+    }
+
+}
+// const c1 = new car ("BMW",85974623,"Blue")
+// c1.display()
+const c2 = new BMW ("wheels","power", "BMW",85974623,"Blue");
+c2.display()
