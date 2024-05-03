a2d211eef80d1602d85aad30ca713da55e2f1308
Abhidev1070 <abhishekdewda506@gmail.com>
2024-05-03 17:29:15 +0530

    binarySearch

--- day49_js_20_constrcut/App.js
+++ day49_js_20_constrcut/App.js
@@ -420,7 +420,7 @@ console.log(z);*/
 //     if (i % 2 === 0) {
 //         if (i % 4 === 0) {
 //             if (i % 6 === 0) {
-               
+
 //             }
 //         }
 //     }
@@ -435,7 +435,33 @@ console.log(z);*/
 // const z=(x>y)?x:y;
 // console.log(z);
 
+//binaryseach number 
 
+const binarySearch = (arr, target) => {
+    let s = 0;
+    let e = arr.length - 1;
+    let mid;
+
+
+    while (s <= e) {
+        mid = Math.floor((s + e) / 2);
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] > target) {
+            e = mid - 1;
+        }
+        else if (arr[mid] < target) {
+            s = mid + 1;
+        }
+    }
+    return -1;
+};
+// main function
+const arr = [10, 20, 30, 500, 750, 5000, 6954, 55500]
+let target = 750;
+const result = binarySearch(arr, target);
+console.log(result);
 
 
 
