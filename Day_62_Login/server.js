bcc2232adfd6a281ea21a3759319dee447f76cfd
Abhidev1070 <abhishekdewda506@gmail.com>
2024-05-22 14:52:48 +0530

    file hendling login pegas

---
+++ Day_62_Login/server.js
@@ -0,0 +1,41 @@
+import express from "express"
+const app = express();
+app.use(express.urlencoded({extended:true}));
+
+let user =[];
+
+app.get('/',(req,res)=>{
+    res.render('./register.ejs')
+})
+app.post('/register',(req,res)=>{
+    const {name,email,password} = req.body;
+    user.push({name,email,password});
+    res.redirect('/login')
+})
+
+
+app.get('/login',(req,res)=>{
+    res.render('./login.ejs')
+})
+
+app.post('/login',(req,res)=>{
+    const {password,email}=req.body;
+
+    for(let i=0; i<user.length;i++){
+        if(user[i].password==password && user[i].email == email){
+            res.redirect('/user')
+        }
+        else{
+            res.redirect('/error')
+        }
+    }
+})
+
+app.get('/user',(req,res)=>{
+    res.render('./user.ejs')
+})
+app.get('/user,',(req,res)=>{
+    res.send('error')
+})
+
+app.listen(2000,()=>console.log(`server is running 0n oprot 2000`))
