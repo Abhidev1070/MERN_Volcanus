53dbce29161b3d0fe5de8a2a908a5c6480f10b5a
Abhidev1070 <abhishekdewda506@gmail.com>
2024-06-06 12:59:35 +0530

    getproductBy Id

--- Day_65_API/Controllers/product.js
+++ Day_65_API/Controllers/product.js
@@ -3,6 +3,8 @@ import { Products } from "../Models/Products.js";
 export const addProduct =  async (req,res)=>{
     const {title,description,price,qty,imgSrc,category}=req.body;
 
+    // add product 
+
     let product = await Products.create({
         title,
         description,
@@ -31,6 +33,29 @@ export const getAllProduct =async(req,res)=>{
         
      }
 }
+
+// get product by id 
+export const getProductById =async (req,res)=>{
+    const id = req.params.id;
+
+    let product = await Products.findById(id)
+    res.json({massage:'product  By Id  ',product})
+
+    
+}
+
+
+// update product by id 
+export const updateById =async (req,res)=>{
+    const id = req.params.id;
+    let product  = await Products.findByIdAndUpdate(id,req.body,{new:true})
+    if(!product) return res.json({massage:"invaild id "});
+    res.json({massage: "your  prodcut by ipdate ",product})
+
+
+}
+    
+
 
 // home
  export const home = async(req,res)=>{
--- Day_65_API/Routes/product.js
+++ Day_65_API/Routes/product.js
@@ -1,12 +1,40 @@
 import express from 'express'
-import { addProduct,getAllProduct, home } from '../Controllers/product.js';
+import { addProduct,getAllProduct, home,getProductById,updateById } from '../Controllers/product.js';
 
 const router = express.Router();
 
 // add product 
+//@method -post 
+//@route-// api/product/add
+router.post("/add",addProduct); 
 
-router.post("/add",addProduct); // api/product/add
-router.get("/get",getAllProduct)
+// aget product 
+//@method -get
+//@route-// api/product/get
+
+router.get("/get",getAllProduct) // api/product/get
+
+
+// get product by id  
+//@method - get 
+//@route-// api/product/get
+router.get("/:id",getProductById)
+
+
+// update product by id  
+//@method - put 
+//@route-// api/product/id
+router.put("/:id",updateById)
+
+
+
+
+
+
+
+//home product 
+//@method -home 
+//@route-// api/product/home
 router.get("/home",home) // api/product/home
 
 export default router;
