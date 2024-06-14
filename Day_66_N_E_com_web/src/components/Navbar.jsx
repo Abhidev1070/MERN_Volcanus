a936c6a4085cd279ebdfc6c5b868f367d80de062
Abhidev1070 <abhishekdewda506@gmail.com>
2024-06-14 20:30:29 +0530

    add to cart

--- Day_66_N_E_com_web/src/components/Navbar.jsx
+++ Day_66_N_E_com_web/src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import img_3 from "../assets/img_3.jpg"
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="nav_bar">
+      <div className="beand">
+        <img src={img_3} alt=""  />
+      </div>
+      <div className="search_bar">
+      <FaSearch /><input type="text" placeholder="Search prodact" />
+      </div>
+      <div className="cart">
+        <div className='cart_0'>o</div>
+
+        < FaShoppingCart/>
+
+      </div>
+    </div>
+    
+    </>
   )
 }
 
