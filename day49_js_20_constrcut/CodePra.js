af73cefec35e62d425d89af528cd0105fdfbed2a
Abhidev1070 <abhishekdewda506@gmail.com>
2024-08-10 14:27:43 +0530

    prime number

--- day49_js_20_constrcut/CodePra.js
+++ day49_js_20_constrcut/CodePra.js
@@ -42,14 +42,38 @@
 
 // question 4
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-const sum = arr.reduce((previousSum, element) => {
-  const newSum = previousSum + element;
-//   console.log(previousSum);
-//   console.log(element);
-  return newSum;
-}, 0); 
+// const sum = arr.reduce((previousSum, element) => {
+//   const newSum = previousSum + element;
+// //   console.log(previousSum);
+// //   console.log(element);
+//   return newSum;
+// }, 0); 
 
-console.log(sum); 
+// console.log(sum); 
+
+/// question 5
+
+function isPrime(num) { 
+   if (num <= 1) return false; 
+ 
+   for (let i = 2; i <= Math.sqrt(num); i++) { 
+     if (num % i === 0) return false; 
+   } 
+ 
+   return true; 
+ }
+ 
+ // Test the function with a range of numbers
+
+ function testPrimes() {
+   const numbersToTest = [0, 1, 2, 3, 4, 5, 10, 13, 17, 18, 19, 20];
+   numbersToTest.forEach((num) => {
+     console.log(`Is ${num} a prime number? ${isPrime(num)}`);
+   });
+ }
+ 
+ testPrimes();
+ 
 
