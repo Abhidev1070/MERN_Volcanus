afe8e5693c2b9534eef68bf3881d70e4fa2dfd8d
Abhidev1070 <abhishekdewda506@gmail.com>
2024-09-21 14:15:43 +0530

    replace splic.reverse.join

--- day49_js_20_constrcut/CodePra.js
+++ day49_js_20_constrcut/CodePra.js
@@ -446,9 +446,24 @@
 
 ///question 27
 
-let sayHello = function (name) {
-  console.log(`Hello ${name}`);
-};
+// let sayHello = function (name) {
+//   console.log(`Hello ${name}`);
+// };
 
-sayHello('Chris'); 
+// sayHello('Chris'); 
+
+
+///question 28
+function isPalindrome(str) {
+ 
+  const cleanedStr = str.replace(/[^\w]/g, "").toLowerCase();
+  
+  const reversedStr = cleanedStr.split("").reverse().join("");
+  
+  return cleanedStr === reversedStr;
+}
+console.log(isPalindrome("A man, a plan, a canal, Panama"));  
+console.log(isPalindrome("Hello"));  
+
+
 
