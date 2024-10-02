458b9ca95e8b9887e555a196a9b044f080df12f2
Abhidev1070 <abhishekdewda506@gmail.com>
2024-10-02 15:14:43 +0530

    parrtan

--- day49_js_20_constrcut/CodePra.js
+++ day49_js_20_constrcut/CodePra.js
@@ -603,18 +603,47 @@
 ///question 36
 
 
-let n = 7;
-let output = ""; 
+// let n = 7;
+// let output = ""; 
+
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n-i; j++) {
+//         if (i == Math.floor(n / 2) || j == Math.floor(n / 2)) {
+//             output += "*"; 
+//         } else {
+//             output += " ";
+//         }
+//     }
+//     output += "\n"; 
+// }
+
+// console.log(output);
+
+
+
+///question 37
+
+let n = 5;
+let string = "";
 
 for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n-i; j++) {
-        if (i == Math.floor(n / 2) || j == Math.floor(n / 2)) {
-            output += "*"; 
-        } else {
-            output += " ";
-        }
-    }
-    output += "\n"; 
+  for (let j = 0; j < i; j++) {
+    string += " ";
+  }
+  for (let k = 0; k < (n - i) * 2 - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
 }
+// pyramid pattern
+for (let i = 2; i <= n; i++) {
+  for (let j = n; j > i; j--) {
+    string += " ";
+  }
 
-console.log(output);
+  for (let k = 0; k < i * 2 - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
