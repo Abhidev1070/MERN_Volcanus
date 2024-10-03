a382bcf95ed3266bda235fb3b674b43d53f9aea4
Abhidev1070 <abhishekdewda506@gmail.com>
2024-10-03 14:55:47 +0530

    star

--- day49_js_20_constrcut/CodePra.js
+++ day49_js_20_constrcut/CodePra.js
@@ -623,27 +623,42 @@
 
 ///question 37
 
-let n = 5;
-let string = "";
+// let n = 5;
+// let string = "";
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += " ";
-  }
-  for (let k = 0; k < (n - i) * 2 - 1; k++) {
-    string += "*";
-  }
-  string += "\n";
-}
-// pyramid pattern
-for (let i = 2; i <= n; i++) {
-  for (let j = n; j > i; j--) {
-    string += " ";
-  }
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 0; k < (n - i) * 2 - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
 
-  for (let k = 0; k < i * 2 - 1; k++) {
-    string += "*";
+// for (let i = 2; i <= n; i++) {
+//   for (let j = n; j > i; j--) {
+//     string += " ";
+//   }
+
+//   for (let k = 0; k < i * 2 - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+///question 38
+let i =10;
+pattern = "";
+for (let n = 0; n <= i; n++) {
+  for (let num = i; num <i+n; num++) {
+     pattern += "*";
+    
+    
   }
-  string += "\n";
+  pattern += "\n";
 }
-console.log(string);
+console.log(pattern);
+
